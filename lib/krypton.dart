62c13ffc0c6982839bdd35cf5477655f0901af63
Nicolas Gimenez <nicolas.gimenez@baozi.technology>
2020-04-28 00:56:52 +0200

    Export private files from API file like in dart_style package

---
+++ lib/krypton.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2020, the Krypton project authors.
+// For full authorship information, refer to the version control history at
+// https://github.com/krypton-org/krypton-flutter
+// All rights reserved. Use of this source code is governed by the
+// MIT license that can be found in the LICENSE file.
+
+library krypton;
+
+export 'src/krypton.dart';
