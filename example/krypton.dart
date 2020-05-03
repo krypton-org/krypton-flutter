819069513817a73017c24165b798cc5b57474db7
Nicolas Gimenez <nicolas.gimenez@baozi.technology>
2020-05-04 00:32:03 +0200

    Add register

---
+++ example/krypton.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2020, the Krypton project authors.
+// For full authorship information, refer to the version control history at
+// https://github.com/krypton-org/krypton-flutter or visit the AUTHORS file.
+// All rights reserved. Use of this source code is governed by the
+// MIT license that can be found in the LICENSE file.
+
+library krypton.example.krypton;
+
+import 'package:krypton/krypton.dart';
+// import 'package:dart_style/src/debug.dart' as debug;
+
+void main(List<String> args) {
+  // Enable debugging so you can see some of the formatter's internal state.
+  // Normal users do not do this.
+  // debug.traceChunkBuilder = true;
+  // debug.traceLineWriter = true;
+  // debug.traceSplitter = true;
+  // debug.useAnsiColors = true;
+
+  KryptonClient kryptonClient =
+      KryptonClient.fromString("https://localhost:5000");
+  kryptonClient.register("nicolas@example.com", "1234unsecurepassword");
+}
--- test/krypton_test.dart
+++ test/krypton_test.dart
@@ -1,6 +1,6 @@
 // Copyright (c) 2020, the Krypton project authors.
 // For full authorship information, refer to the version control history at
-// https://github.com/krypton-org/krypton-flutter
+// https://github.com/krypton-org/krypton-flutter or visit the AUTHORS file.
 // All rights reserved. Use of this source code is governed by the
 // MIT license that can be found in the LICENSE file.
 
@@ -10,16 +10,12 @@ import 'package:krypton/krypton.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const _kryptonClient = KryptonClient("http://localhost:5000");
+  final _kryptonClient = KryptonClient("http://localhost:5000");
 
+  /// This test should not throw any exception
   test('register a user with basic email password and without optional fields',
       () async {
-    try {
-      final isRegistered = await _kryptonClient.register(
+      await _kryptonClient.register(
           'john.doe@example.com', 'iAmavalidPassw0rd');
-      expect(isRegistered, equals(true));
-    } on FormatException catch (err) {
-      print(err);
-    }
   });
 }
