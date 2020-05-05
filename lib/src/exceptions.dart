e56cc31a0134b74bf6ef6e06fe7d9ccf3edf540e
Nicolas Gimenez <nicolas.gimenez@baozi.technology>
2020-05-05 03:11:26 +0200

    Add exceptions file

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2020, the Krypton project authors.
+// For full authorship information, refer to the version control history at
+// https://github.com/krypton-org/krypton-flutter or visit the AUTHORS file.
+// All rights reserved. Use of this source code is governed by the
+// MIT license that can be found in the LICENSE file.
+
+library krypton.src.exceptions;
+
+class KryptonException implements Exception {
+}
+
+class AlreadyLoggedInException extends KryptonException {
+ 
+}
